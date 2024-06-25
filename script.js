984fa523c1364c140c9e3233e88565ffc758c25c
serikrvt <serikrvt@gmail.com>
2024-06-25 20:02:47 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const users = [
+  { id: 1, name: 'Alice', age: 25, city: 'New York' },
+  { id: 2, name: 'Bob', age: 30, city: 'Los Angeles' },
+  { id: 3, name: 'Charlie', age: 28, city: 'Chicago' },
+  { id: 4, name: 'David', age: 35, city: 'Houston' },
+  { id: 5, name: 'Alice', age: 20, city: 'London' },
+  { id: 6, name: 'Mark', age: 18, city: 'London' },
+];
+
+let fruits = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry'];
+ const elem = document.querySelectorAll('.highlight')
+ console.log(elem);
+ 
+ for ( let i = 0;i < elem.length; i++ ){
+  elem[i].style.backgroundColor = 'yellow'
+ }
+
+g
+ 
+
+// Напишите код, который находит все элементы с классом highlight и изменяет их цвет фона на желтый.
+
+// Создайте список <ul>. Добавьте в него пять элементов <li> с текстом "Элемент 1", "Элемент 2" и так далее. Каждый нечетный элемент списка должен иметь класс odd и синий цвет текста.
+
+// Создайте кнопку с текстом "Изменить цвет". При нажатии на кнопку все элементы с классом change-color должны поменять свой цвет текста на красный.
+
+// Создайте форму с полем ввода текста и кнопкой "Добавить". При нажатии на кнопку в конец списка <ul> должен добавляться новый элемент <li> с текстом из поля ввода.
+
+// Сделайте элемент видимым или скрытым при нажатии на кнопку.
+
+{/* <div id="toggle-element">Этот элемент можно скрыть</div>
+  <button id="toggle-button">Скрыть/Показать элемент</button> */}
+
+// Измените атрибут src изображения при нажатии на кнопку.
+
+{/* <img id="image" src="old-image.jpg" alt="Старое изображение" width="300">
+<button id="change-image-button">Изменить изображение</button> */}
